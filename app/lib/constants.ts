5cd0f29d2c7eb9d390473ced2eb60cb3cb4d1e8c
Abdul-Hafiz <abdulhafizaderemi@gmail.com>
2026-05-31 11:44:09 +0100

    feat: design tokens, Sora+Inter fonts, site metadata, image config

---
+++ app/lib/constants.ts
@@ -0,0 +1,5 @@
+export const PHONE = "[PHONE]"
+export const WHATSAPP_NUMBER = "234[PHONE_WITHOUT_LEADING_ZERO]"
+export const WHATSAPP_MESSAGE =
+  "Hi, I'm interested in a solar installation. Please can you help me?"
+export const WHATSAPP_URL = `https://wa.me/${WHATSAPP_NUMBER}?text=${encodeURIComponent(WHATSAPP_MESSAGE)}`
